dae7ca709638f33fb2e21db78308f790f93a3892
sakhadib <adibsakhawat@iut-dhaka.edu>
2024-08-27 03:40:11 +0600

    Add signup route and controller

---
+++ resources/views/authentication/signup.blade.php
@@ -0,0 +1,119 @@
+@extends('layouts.main')
+
+@section('main')
+
+<div class="vh-10"></div>
+
+<div class="main-page df jcc aic">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 df jcc aic">
+                <img src="rsx/logo/tp_black.png" alt="" style="width: 70%">
+            </div>
+            <div class="col-md-5 offset-md-1 df dfc jcc">
+                {{-- <div class="row">
+                    <div class="col">
+                        <img src="" alt="" style="width: 200px;">
+                    </div>
+                </div> --}}
+                <div class="row">
+                    <div class="col-12">
+                        <h1 class="display-6">Welcome to muktobidya</h1>
+                        <p class="lead">Here starts your great journey of self-learnig.</p>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col-12">
+                        <form action="">
+                            @csrf
+                            <div class="form-box mb-3">
+                                <input class="form-control" type="text" name="username" required placeholder="username">
+                            </div>
+                            <div class="form-box mb-3">
+                                <input class="form-control" type="email" name="email" required placeholder="Email">
+                            </div>
+                            <div class="form-box mb-3">
+                                <input class="form-control" type="text" name="FullName" required placeholder="Full Name">
+                            </div>
+                            <div class="form-box mb-3">
+                                <input class="form-control" type="password" name="password" required placeholder="password">
+                            </div>
+                            <div class="form-box mb-3">
+                                <input class="form-control" type="password" name="conform_password" required placeholder="Confirm Password">
+                            </div>
+                            <button class="amar-btn-outline mb-3 w-100" type="submit">Signup</button>
+                            <p class="lead">
+                                Already have an account? <a class="link-dark" href="/login">Login</a>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<style>
+
+    body{
+        background-color: #fafafa;
+    }
+    .main-page{
+        min-height: 90vh;
+    }
+
+    .form-box{
+        background-color: #fff;
+        /* box-shadow: 0 0 10px rgba(0,0,0,0.1); */
+        padding: 10px;
+    }
+
+    .form-control{
+        border: none;
+        font-size: 1.3rem;
+    }
+
+    .form-control:focus{
+        border: none;
+        box-shadow: none;
+    }
+
+    .amar-btn{
+        background-color: rgb(225, 225, 225);
+        color: #414042;
+        font-size: 1.3rem;
+        padding: 20px;
+        padding-top: 10px;
+        padding-bottom: 15px;
+        border: none;
+        margin-right: 10px;
+        /* border-radius: 5px; */
+    }
+
+    .amar-btn:hover{
+        background-color: #414042;
+        color: white;
+    }
+
+    .amar-btn-outline{
+        background-color: transparent;
+        color: #414042;
+        font-size: 1.3rem;
+        padding: 20px;
+        padding-top: 10px;
+        padding-bottom: 15px;
+        border: 1px solid #414042;
+        margin-right: 10px;
+        /* border-radius: 5px; */
+    }
+
+    .amar-btn-outline:hover{
+        background-color: #414042;
+        color: white;
+    }
+</style>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\signup_Controller;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/signup', [signup_Controller::class, 'index']);
